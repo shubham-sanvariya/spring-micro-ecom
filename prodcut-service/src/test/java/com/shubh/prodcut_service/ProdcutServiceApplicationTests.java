19a8ec3438a999f432d8fb891f23506418d3be4c
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-10 16:55:23 +0530

    adding container config and setting up rest assured

--- prodcut-service/src/test/java/com/shubh/prodcut_service/ProdcutServiceApplicationTests.java
+++ prodcut-service/src/test/java/com/shubh/prodcut_service/ProdcutServiceApplicationTests.java
@@ -1,15 +1,32 @@
 package com.shubh.prodcut_service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+import io.restassured.RestAssured;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProdcutServiceApplicationTests {
 
+	static MongoDBContainer MongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+
 	}
 
 }
