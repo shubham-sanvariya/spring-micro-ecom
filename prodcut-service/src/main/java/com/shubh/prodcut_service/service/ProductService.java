db6c12ebec7b67406776edf8a283726ff7177b58
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-10 16:26:27 +0530

    adding createProduct in product service

--- prodcut-service/src/main/java/com/shubh/prodcut_service/service/ProductService.java
+++ prodcut-service/src/main/java/com/shubh/prodcut_service/service/ProductService.java
@@ -2,13 +2,30 @@ package com.shubh.prodcut_service.service;
 
 import org.springframework.stereotype.Service;
 
+import com.shubh.prodcut_service.dto.ProductRequest;
+import com.shubh.prodcut_service.model.Product;
 import com.shubh.prodcut_service.repository.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
-    
+
+
+
     private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product created successfully");
+    }
 }
