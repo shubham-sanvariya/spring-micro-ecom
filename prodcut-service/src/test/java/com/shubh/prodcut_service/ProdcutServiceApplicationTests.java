634149d13688e38ccd02fb242e1b0b78b434d20d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-10 17:37:00 +0530

    adding test shouldCreateProduct

--- prodcut-service/src/test/java/com/shubh/prodcut_service/ProdcutServiceApplicationTests.java
+++ prodcut-service/src/test/java/com/shubh/prodcut_service/ProdcutServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.shubh.prodcut_service;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MongoDBContainer;
 
@@ -13,20 +15,44 @@ import io.restassured.RestAssured;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProdcutServiceApplicationTests {
 
-	static MongoDBContainer MongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+	// by adding this we don't need to manually specify the mongodb uri
+	// spring boot will inject the relevant uri
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
 
 	@LocalServerPort
 	private Integer port;
 
 	@BeforeEach
-	void setup(){
+	void setup() {
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = port;
 	}
 
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
 	void shouldCreateProduct() {
+		String requestBody = """
+				{
+				    "name" : "iphone 15",
+				    "description" : "a garbage phone",
+				    "price" : 1000
+				}
+				""";
 
+		RestAssured.given().contentType("application/json")
+		.body(requestBody)
+		.when()
+		.post("/api/product")
+		.then()
+		.statusCode(201)
+		.body("id",Matchers.notNullValue())
+		.body("name",Matchers.equalTo("iphone 15"))
+		.body("description",Matchers.equalTo("a garbage phone"))
+		.body("price",Matchers.equalTo(1000));
 	}
 
 }
