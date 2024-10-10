ca8ce742cc1ce89d9ae548e19856de3a03618f85
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-10 16:26:45 +0530

    adding Product controller

---
+++ prodcut-service/src/main/java/com/shubh/prodcut_service/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.shubh.prodcut_service.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shubh.prodcut_service.dto.ProductRequest;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+    
+    
+}
