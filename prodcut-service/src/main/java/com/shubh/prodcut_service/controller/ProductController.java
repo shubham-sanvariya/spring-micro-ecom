4094caf1539273def90cdd0aa66e4add709d4882
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-10 16:29:03 +0530

    adding createProduct method mapping in controller

--- prodcut-service/src/main/java/com/shubh/prodcut_service/controller/ProductController.java
+++ prodcut-service/src/main/java/com/shubh/prodcut_service/controller/ProductController.java
@@ -8,10 +8,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shubh.prodcut_service.dto.ProductRequest;
+import com.shubh.prodcut_service.model.Product;
+import com.shubh.prodcut_service.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/product")
 public class ProductController {
-    
-    
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
 }
--- prodcut-service/src/main/java/com/shubh/prodcut_service/service/ProductService.java
+++ prodcut-service/src/main/java/com/shubh/prodcut_service/service/ProductService.java
@@ -14,18 +14,16 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ProductService {
 
-
-
     private final ProductRepository productRepository;
 
-    public void createProduct(ProductRequest productRequest) {
+    public Product createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.name())
                 .description(productRequest.description())
                 .price(productRequest.price())
                 .build();
 
-        productRepository.save(product);
         log.info("Product created successfully");
+        return productRepository.save(product);
     }
 }
