d645035706000625e3502c1a659ccddfcc3b23f8
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-10 16:46:55 +0530

    adding get all products mapping in product controller

--- prodcut-service/src/main/java/com/shubh/prodcut_service/controller/ProductController.java
+++ prodcut-service/src/main/java/com/shubh/prodcut_service/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.shubh.prodcut_service.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shubh.prodcut_service.dto.ProductRequest;
-import com.shubh.prodcut_service.model.Product;
+import com.shubh.prodcut_service.dto.ProductResponse;
 import com.shubh.prodcut_service.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,7 +25,13 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Product createProduct(@RequestBody ProductRequest productRequest) {
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
         return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
