394779c6a868a81f4bf4b6cd29b6f91c04a5aa77
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-12 18:24:07 +0530

    adding place order method in order service

--- order-service/src/main/java/com/shubh/order_service/service/OrderService.java
+++ order-service/src/main/java/com/shubh/order_service/service/OrderService.java
@@ -1,9 +1,28 @@
 package com.shubh.order_service.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import com.shubh.order_service.dto.OrderRequest;
+import com.shubh.order_service.model.Order;
+import com.shubh.order_service.repository.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private final OrderRepository orderRepository;
     
-    
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+
+        orderRepository.save(order);
+    }
 }
