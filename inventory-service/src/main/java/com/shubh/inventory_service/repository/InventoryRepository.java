91efbc5405f0539e3b41f462bbae93c6a8726155
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-13 14:13:57 +0530

    adding inventory repository

---
+++ inventory-service/src/main/java/com/shubh/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.shubh.inventory_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shubh.inventory_service.model.Inventory;
+
+public interface InventoryRepository extends JpaRepository<Inventory,Long>{
+    
+}
