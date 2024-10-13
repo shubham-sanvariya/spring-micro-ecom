2ba129049064d93c070192c1a4250fa05dd03036
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-13 15:58:39 +0530

    checking if product is int stock

--- order-service/src/main/java/com/shubh/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/shubh/order_service/client/InventoryClient.java
@@ -2,8 +2,6 @@ package com.shubh.order_service.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "inventory", url = "http://localhost:8082")
--- order-service/src/main/java/com/shubh/order_service/service/OrderService.java
+++ order-service/src/main/java/com/shubh/order_service/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.shubh.order_service.client.InventoryClient;
 import com.shubh.order_service.dto.OrderRequest;
 import com.shubh.order_service.model.Order;
 import com.shubh.order_service.repository.OrderRepository;
@@ -15,14 +16,25 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
     
     public void placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+
+       var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
+
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
 
         orderRepository.save(order);
+
+        }else{
+            throw new RuntimeException("product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+        }
+        
+
     }
 }
