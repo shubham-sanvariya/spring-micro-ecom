6887a47d425955d67a142eb5ec24e7675667fc92
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-13 15:57:50 +0530

    adding inventory client using feign

--- order-service/src/main/java/com/shubh/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/shubh/order_service/client/InventoryClient.java
@@ -1,8 +1,15 @@
 package com.shubh.order_service.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "inventory", url = "http://localhost:8082")
 public interface InventoryClient {
     
+    // @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @GetMapping("/api/inventory")
+    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
