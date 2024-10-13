b94ae49c010c551764507e05e46ebb043c134320
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-13 14:21:29 +0530

    adding inventory controller

---
+++ inventory-service/src/main/java/com/shubh/inventory_service/controller/InventoryController.java
@@ -0,0 +1,10 @@
+package com.shubh.inventory_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+    
+}
