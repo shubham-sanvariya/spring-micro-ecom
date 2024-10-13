5228d58ac3de629f152ad6bc8ac8aa088fdaa2cc
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-13 21:48:37 +0530

    adding external docs for open api

--- prodcut-service/src/main/java/com/shubh/prodcut_service/config/OpenAPIConfig.java
+++ prodcut-service/src/main/java/com/shubh/prodcut_service/config/OpenAPIConfig.java
@@ -1,8 +1,26 @@
 package com.shubh.prodcut_service.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class OpenAPIConfig {
-    
+
+    @Bean
+    public OpenAPI productServicesAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Product Service API")
+                        .description("This is the rest api for Product Service")
+                        .version("v.0.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("You can reffer to the Product service Wiki Documentation")
+                        .url("https://product-service-dummy-url.com/docs"));
+    }
 }
