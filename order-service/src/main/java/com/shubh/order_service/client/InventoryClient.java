944ffdeb27ebeb562ebbc83a71c2451b9f56d493
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-13 23:11:07 +0530

    adding GetExchange instead of getmapping

--- order-service/src/main/java/com/shubh/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/shubh/order_service/client/InventoryClient.java
@@ -1,13 +1,11 @@
 package com.shubh.order_service.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
     
     // @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
-    @GetMapping("/api/inventory")
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
