91d6ca771944aca8f2683b6a92fc449771e2dfda
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-13 17:12:31 +0530

    adding shouldFailOrderWhenProductIsNotInStock

--- order-service/src/test/java/com/shubh/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/shubh/order_service/OrderServiceApplicationTests.java
@@ -1,26 +1,27 @@
 package com.shubh.order_service;
 
-import static org.junit.Assert.assertThat;
 
+// import com.techie.microservices.order.stubs.InventoryClientStub;
+import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Import;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.MySQLContainer;
 
-import io.restassured.RestAssured;
+import com.shubh.order_service.stubs.InventoryClientStub;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
-	static MySQLContainer mySQLContainer = new MySQLContainer<>("mysql:8.3.0");
-
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
 	@LocalServerPort
 	private Integer port;
 
@@ -37,25 +38,46 @@ class OrderServiceApplicationTests {
 	@Test
 	void shouldSubmitOrder() {
 		String submitOrderJson = """
-				 {
-				                 "skuCode": "iphone_15",
-				                 "price": 1000,
-				                 "quantity": 1
-				            }
+				{
+				     "skuCode": "iphone_15",
+				     "price": 1000,
+				     "quantity": 1
+				}
 				""";
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
 
 		var responseBodyString = RestAssured.given()
-			.contentType("application/json")
-			.body(submitOrderJson)
-		.when()
-		.post("/api/order")
-		.then()
-		.log().all()
-		.statusCode(201)
-		.extract()
-		.body().asString();
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
 
-		assertThat(responseBodyString,Matchers.is("Order Placed Successfully"));
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
 	}
 
+	@Test
+	void shouldFailOrderWhenProductIsNotInStock() {
+		String submitOrderJson = """
+				{
+				     "skuCode": "iphone_15",
+				     "price": 1000,
+				     "quantity": 1000
+				}
+				""";
+		InventoryClientStub.stubInventoryCall("iphone_15", 1000);
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(500);
+	}
 }
