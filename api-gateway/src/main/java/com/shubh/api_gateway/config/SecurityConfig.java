208450cda6039f10a1bb868f62463b24832f0620
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-13 22:56:09 +0530

    adding request matchers

--- api-gateway/src/main/java/com/shubh/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/shubh/api_gateway/config/SecurityConfig.java
@@ -9,10 +9,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] freeResourceUrls = { "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus" };
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers(freeResourceUrls).permitAll()
+                .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
 
