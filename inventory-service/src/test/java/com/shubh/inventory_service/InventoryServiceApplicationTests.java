90e09d6466517bd6defd6c847d9db4048d3b27a5
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-13 14:53:24 +0530

    adding integration test

--- inventory-service/src/test/java/com/shubh/inventory_service/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/shubh/inventory_service/InventoryServiceApplicationTests.java
@@ -1,15 +1,59 @@
 package com.shubh.inventory_service;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+
+import io.restassured.RestAssured;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class InventoryServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer<>("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup(){
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mySQLContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldReadInventory(){
+		var response = RestAssured.given()
+			.when()
+			.get("/api/inventory?skuCode=iphone_15&quantity=1")
+			.then()
+			.log().all()
+			.statusCode(200)
+			.extract().response().as(Boolean.class);
+
+		assertTrue(response);
+
+		var negativeResponse = RestAssured.given()
+				.when()
+				.get("/api/inventory?skuCode=iphone_15&quantity=1000")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().response().as(Boolean.class);
+
+		assertFalse(negativeResponse);
 	}
 
 }
