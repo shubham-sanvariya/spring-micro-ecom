479f5ab23a5ed904ab266c22b62dfa4b6696cd3b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-14 14:25:15 +0530

    adding OrderPlacedEvent class

---
+++ order-service/src/main/java/com/shubh/order_service/event/OrderPlacedEvent.java
@@ -0,0 +1,13 @@
+package com.shubh.order_service.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderPlacedEvent {
+    private String orderNumber;
+    private String email;
+}
