0c0477998a3d4e29bf3f263fbbd4d5ebe50d6a19
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-14 12:15:39 +0530

    adding fallback method for invnetory serice

--- order-service/src/main/java/com/shubh/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/shubh/order_service/client/InventoryClient.java
@@ -1,11 +1,28 @@
 package com.shubh.order_service.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
+import groovy.util.logging.Slf4j;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+
+@Slf4j
 public interface InventoryClient {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
     
     // @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    // this is the name in the application properties
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable){
+        log.info("Cannot get inventory for skucode {}, failure reason: {}",code, throwable.getMessage());
+        return false;
+    }
 }
