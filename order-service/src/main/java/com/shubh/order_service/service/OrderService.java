a22343163e9972a45cd450c68ccc0aa5bfef52fd
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-10-14 17:14:48 +0530

    implementing new avro Order placed Event file in order servcie

--- order-service/src/main/java/com/shubh/order_service/service/OrderService.java
+++ order-service/src/main/java/com/shubh/order_service/service/OrderService.java
@@ -37,7 +37,11 @@ public class OrderService {
         orderRepository.save(order);
 
         // send the message to kafka Topic
-        OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(),orderRequest.userDetails().email());
+        OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+        orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+        orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+        orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+        orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
         log.info("Start - Sending OrderPlacedEvent {} to kafka topic order-placed",orderPlacedEvent);
         kafkaTemplate.send("order-placed",orderPlacedEvent);
         log.info("End - Sending OrderPlacedEvent {} to kafka topic order-placed", orderPlacedEvent);
